dfdf8191828eedfd4877a4e4120f81f54295d58b
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 22:19:32 -0600

    Update todolist.php

--- todolist.php
+++ todolist.php
@@ -4,6 +4,14 @@ require_once("model-todolist.php");
 
 $pageTitle = "todolist";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType'])){
+    case "Add":
+    insertTodolist($_POST['TaskName'], $_POST['Priority'], $_POST['DueDate'], $_POST['Status']);
+    break;
+  }
+}
 $todolist = selectTodolist();
 include "view-todolist.php";
 include "view-footer.php";
