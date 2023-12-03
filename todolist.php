6954523e98209c734ef1fa89cf8a42f01ea894e3
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 22:33:28 -0600

    Update todolist.php

--- todolist.php
+++ todolist.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']){
     case "Add":
-    insertTodolist($_POST['TaskName'], $_POST['Priority'], $_POST['DueDate'], $_POST['Status']);
+    if (insertTodolist($_POST['TaskName'], $_POST['Priority'], $_POST['DueDate'], $_POST['Status'])){
+      echo '<div class="alert alert-success" role="alert"> Task Added! </div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+    }
     break;
   }
 }
