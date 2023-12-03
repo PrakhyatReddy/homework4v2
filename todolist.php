c90d6062900ef8ca03a0a1af49a5c5c34bb5973c
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 20:41:38 -0600

    Create todolist.php

---
+++ todolist.php
@@ -0,0 +1,10 @@
+<?php
+require_once(require_once("util-db.php");
+require_once("model-todolist.php");
+
+$pageTitle = "todolist";
+include "view-header.php";
+$todolist = selectTodolist();
+include "view-todolist.php";
+include "view-footer.php";
+?>
