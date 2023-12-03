e60be3766498455d7949bd639aa7d298fd07c9ef
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 21:22:31 -0600

    Update model-todolist.php

--- model-todolist.php
+++ model-todolist.php
@@ -2,7 +2,7 @@
 function selectTodolist() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM 'ToDoList'");
+        $stmt = $conn->prepare("SELECT TaskID, TaskName, Priority, DueDate, Status FROM 'ToDoList'");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
