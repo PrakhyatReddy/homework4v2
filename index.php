55881d4ceb45170daa3fbb6bdabe6d6ff6ff2348
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 20:31:00 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+
+<div class="container text-center my-5">
+    <h1>Welcome to Homework 4</h1>
+    <p>Your one stop shop for tacking your life</p>
+    <a href="tasks.php" class="btn btn-primary mt-3">Tasks</a>
+    <a href="comments.php" class="btn btn-primary mt-3">Comments</a>
+</div>
+
+<?php
+include "view-footer.php";
+?>
