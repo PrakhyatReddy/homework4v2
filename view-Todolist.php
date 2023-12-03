303ee0c691dd09690cdbdcac865c536b2823c283
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 21:10:01 -0600

    Create view-Todolist.php

---
+++ view-Todolist.php
@@ -0,0 +1,30 @@
+<h1>Welcome to your To Do List</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Task ID</th>
+        <th>Task Name</th>
+        <th>Priority</th>
+        <th>Due Date</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php
+        while ($todolistf =$todolist->fetch_assoc()){
+      ?>
+          <tr>
+              <td><?php echo $todolistf['TaskID']; ?></td>
+              <td><?php echo $todolistf['TaskName']; ?></td>
+              <td><?php echo $todolistf['Priority']; ?></td>
+              <td><?php echo $todolistf['DueDate']; ?></td>
+              <td><?php echo $todolistf['Status']; ?></td>
+          </tr>
+      <?php
+        }
+      ?>
+    </tbody>
+  </table>
+</div>
