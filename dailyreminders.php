21e879feda551e03fce5940afac31d555e939d9b
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 21:36:11 -0600

    Update dailyreminders.php

--- dailyreminders.php
+++ dailyreminders.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-dailyreminders.php");
 
+$pageTitle = "todolist";
+include "view-header.php";
+$dailyreminders = selectDailyReminders();
+include "view-todolist.php";
+include "view-footer.php";
+?>
