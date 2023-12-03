bdd381feebb6734c6f00def44ab1bb0cbdc6055b
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 21:35:21 -0600

    Update shoppinglist.php

--- shoppinglist.php
+++ shoppinglist.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-shoppinglist.php");
 
+$pageTitle = "todolist";
+include "view-header.php";
+$shoppinglist = selectShoppingList();
+include "view-todolist.php";
+include "view-footer.php";
+?>
