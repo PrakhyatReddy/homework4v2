c9e429abfa563b61f3b80c30c56dcfe3f7b3285f
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 20:33:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ include "view-header.php";
 <div class="container text-center my-5">
     <h1>Welcome to Homework 4</h1>
     <p>Your one stop shop for tacking your life</p>
-    <a href="tasks.php" class="btn btn-primary mt-3">Tasks</a>
-    <a href="comments.php" class="btn btn-primary mt-3">Comments</a>
 </div>
 
 <?php
