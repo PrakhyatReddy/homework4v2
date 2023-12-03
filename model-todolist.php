1fbc54cc9e18bfd2c2d8c47e4fc78788b86ab5f3
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 20:44:34 -0600

    Update model-todolist.php

--- model-todolist.php
+++ model-todolist.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectTodolist() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
