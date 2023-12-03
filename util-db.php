65c3f811886f0f635906fbd1cf993f5a861a34e9
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 20:36:35 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,12 @@
-
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('159.89.47.44' , 'prakhyat_hw4userv2' , 'LeBumJames', 'prakhyat_homework4v2');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
