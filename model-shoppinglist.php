db47c3c1d544dae01b7662c335df8471792e03e1
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-02 21:39:31 -0600

    Update model-shoppinglist.php

--- model-shoppinglist.php
+++ model-shoppinglist.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectShoppingList() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM `ShoppingList`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
