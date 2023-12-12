14e44c67b64364be62f324afe97209db952dc911
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 00:16:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "view-header.php";
 
 <div class="container text-center my-5">
     <h1>Welcome to Homework 4</h1>
-    <p>Your one stop shop for tacking your life</p>
+    <p>Your one-stop shop for tackling your life</p>
 </div>
 
 <?php
