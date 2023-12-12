558a333bbe3cc2252eeaffd090bb49f104cc63e5
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 00:56:10 -0600

    Update model-shoppinglist.php

--- model-shoppinglist.php
+++ model-shoppinglist.php
@@ -12,6 +12,48 @@ function selectShoppinglist() {
         throw $e;
     }
 }
+
+function insertShoppinglist($tdln, $tdlp, $tdld, $tdls) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `ToDoList` (TaskName, Priority, DueDate, Status) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $tdln, $tdlp, $tdld, $tdls);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateShoppinglist($tdln, $tdlp, $tdld, $tdls, $tdli) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `ToDoList` set `TaskName`=?, `Priority`=?, `DueDate`=?, `Status`=? where `TaskID` = ?");
+        $stmt->bind_param("ssssi", $tdln, $tdlp, $tdld, $tdls, $tdli);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteShoppinglist($tdli) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `ToDoList` where TaskID = ? ");
+        $stmt->bind_param("i", $tdli);
+        $success = $stmt->execute();
+        $conn->close(); 
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
 
 
