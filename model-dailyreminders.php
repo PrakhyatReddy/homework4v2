85c1d386c9ddb4440798b404b8f382b5847dc67f
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-11 20:31:13 -0600

    Update model-dailyreminders.php

--- model-dailyreminders.php
+++ model-dailyreminders.php
@@ -1,1 +1,58 @@
+<?php
+function selectShoppingList() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM `ShoppingList`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
+function insertTodolist($tdln, $tdlp, $tdld, $tdls) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `ToDoList` (TaskName, Priority, DueDate, Status) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $tdln, $tdlp, $tdld, $tdls);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateTodolist($tdln, $tdlp, $tdld, $tdls, $tdli) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `ToDoList` set `TaskName`=?, `Priority`=?, `DueDate`=?, `Status`=? where `TaskID` = ?");
+        $stmt->bind_param("ssssi", $tdln, $tdlp, $tdld, $tdls, $tdli);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteTodolist($tdli) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `ToDoList` where TaskID = ? ");
+        $stmt->bind_param("i", $tdli);
+        $success = $stmt->execute();
+        $conn->close(); 
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
