6ee9eb513ab66cb043fde4982dfa11c5246b143d
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-11 23:35:58 -0600

    Update assignmenttracker.php

--- assignmenttracker.php
+++ assignmenttracker.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-assignmenttracker.php");
 
-$pageTitle = "todolist";
+$pageTitle = "assignmenttracker";
 include "view-header.php";
 $assignmenttracker = selectAssignmenttracker();
 include "view-assignmenttracker.php";
