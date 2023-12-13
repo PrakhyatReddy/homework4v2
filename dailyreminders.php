52612fc74a446425737a1dd0343316d440a292b7
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 22:22:19 -0600

    Update dailyreminders.php

--- dailyreminders.php
+++ dailyreminders.php
@@ -4,6 +4,19 @@ require_once("model-dailyreminders.php");
 
 $pageTitle = "dailyreminders";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertDailyreminders($_POST['ReminderText'], $_POST['ReminderTime'], $_POST['Recurring'])){
+      echo '<div class="alert alert-success" role="alert"> Task Added! </div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+    }
+    break;
+  }
+}
+
 $dailyreminders = selectDailyreminders();
 include "view-dailyreminders.php";
 include "view-footer.php";
