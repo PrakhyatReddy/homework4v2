c929ba8984fe3b200d8a4bf6b76189ff8d56e5f4
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 22:55:03 -0600

    Update assignmenttracker.php

--- assignmenttracker.php
+++ assignmenttracker.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])){
     break;
 
     case "Delete":
-    if (deleteDailyreminders($_POST['AssignmentID'])){
+    if (deleteAssignmenttracker($_POST['AssignmentID'])){
       echo '<div class="alert alert-success" role="alert"> Assignment Deleted! </div>';
     } else {
       echo '<div class="alert alert-danger" role="alert"> Error! </div>';
