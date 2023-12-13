7bbe01b21445272dfb64ad987e6359d5ad8f284d
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 23:01:53 -0600

    Update view-shoppinglist.php

--- view-shoppinglist.php
+++ view-shoppinglist.php
@@ -40,7 +40,7 @@
               </td>
               <td>
                 <form method="post" action="">
-                <input type="hidden" name="tdli" value = "<?php echo $todolistf['TaskID']; ?>">
+                <input type="hidden" name="tdli" value = "<?php echo $shoppinglistf['ItemID']; ?>">
                 <input type="hidden" name="actionType" value="Delete">
                 <button type ="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
