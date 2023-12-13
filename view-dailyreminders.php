143a741f10c1e057fd2f17ad069c54a9652a3e38
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 23:02:20 -0600

    Update view-dailyreminders.php

--- view-dailyreminders.php
+++ view-dailyreminders.php
@@ -38,7 +38,7 @@
               </td>
               <td>
                 <form method="post" action="">
-                <input type="hidden" name="tdli" value = "<?php echo $todolistf['TaskID']; ?>">
+                <input type="hidden" name="tdli" value = "<?php echo $dailyremindersf['ReminderID']; ?>">
                 <input type="hidden" name="actionType" value="Delete">
                 <button type ="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
