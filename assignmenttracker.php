ba1a7f8c25090cccfb9e95b55f6256c55e334de2
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 22:25:28 -0600

    Update assignmenttracker.php

--- assignmenttracker.php
+++ assignmenttracker.php
@@ -4,6 +4,19 @@ require_once("model-assignmenttracker.php");
 
 $pageTitle = "assignmenttracker";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertAssignmenttracker($_POST['AssignmentName'], $_POST['CourseName'], $_POST['DueDate'], $_POST['Status'])){
+      echo '<div class="alert alert-success" role="alert"> Task Added! </div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+    }
+    break;
+  }
+}
+
 $assignmenttracker = selectAssignmenttracker();
 include "view-assignmenttracker.php";
 include "view-footer.php";
