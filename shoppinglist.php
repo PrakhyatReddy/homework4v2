17520c363973d7c583df5330cada5380ecb9fecd
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 22:15:55 -0600

    Update shoppinglist.php

--- shoppinglist.php
+++ shoppinglist.php
@@ -4,6 +4,17 @@ require_once("model-shoppinglist.php");
 
 $pageTitle = "shoppinglist";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertTodolist($_POST['ItemName'], $_POST['Quantity'], $_POST['Category'], $_POST['Purchased'])){
+      echo '<div class="alert alert-success" role="alert"> Task Added! </div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+    }
+    break;
+
 $shoppinglist = selectShoppinglist();
 include "view-shoppinglist.php";
 include "view-footer.php";
