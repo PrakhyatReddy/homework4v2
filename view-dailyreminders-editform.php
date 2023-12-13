30b88acec66a9541a158d66e01cb7a562a571c07
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 23:45:06 -0600

    Update view-dailyreminders-editform.php

--- view-dailyreminders-editform.php
+++ view-dailyreminders-editform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editDailyRemindersModal<?php echo $dailyreminderf['ReminderID']; ?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editDailyRemindersModal<?php echo $dailyremindersf['ReminderID']; ?>">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
@@ -7,30 +7,30 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editDailyRemindersModal<?php echo $dailyreminderf['ReminderID']; ?>" tabindex="-1" aria-labelledby="editDailyRemindersModalLabel<?php echo $dailyreminderf['ReminderID']; ?>" aria-hidden="true">
+<div class="modal fade" id="editDailyRemindersModal<?php echo $dailyremindersf['ReminderID']; ?>" tabindex="-1" aria-labelledby="editDailyRemindersModalLabel<?php echo $dailyremindersf['ReminderID']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editDailyRemindersModalLabel<?php echo $dailyreminderf['ReminderID']; ?>">Edit Daily Reminder</h1>
+        <h1 class="modal-title fs-5" id="editDailyRemindersModalLabel<?php echo $dailyremindersf['ReminderID']; ?>">Edit Daily Reminder</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         
         <form method="post" action="">
           <div class="mb-3">
-            <label for="ReminderText<?php echo $dailyreminderf['ReminderID']; ?>" class="form-label">Reminder Text</label>
-            <input type="text" class="form-control" id="ReminderText<?php echo $dailyreminderf['ReminderID']; ?>" name="ReminderText" value="<?php echo $dailyreminderf['ReminderText']; ?>">
+            <label for="ReminderText<?php echo $dailyremindersf['ReminderID']; ?>" class="form-label">Reminder Text</label>
+            <input type="text" class="form-control" id="ReminderText<?php echo $dailyremindersf['ReminderID']; ?>" name="ReminderText" value="<?php echo $dailyremindersf['ReminderText']; ?>">
           </div>
           <div class="mb-3">
-            <label for="ReminderTime<?php echo $dailyreminderf['ReminderID']; ?>" class="form-label">Reminder Time</label>
-            <input type="text" class="form-control" id="ReminderTime<?php echo $dailyreminderf['ReminderID']; ?>" name="ReminderTime" value="<?php echo $dailyreminderf['ReminderTime']; ?>">
+            <label for="ReminderTime<?php echo $dailyremindersf['ReminderID']; ?>" class="form-label">Reminder Time</label>
+            <input type="text" class="form-control" id="ReminderTime<?php echo $dailyremindersf['ReminderID']; ?>" name="ReminderTime" value="<?php echo $dailyremindersf['ReminderTime']; ?>">
           </div>
           <div class="mb-3">
-            <label for="Recurring<?php echo $dailyreminderf['ReminderID']; ?>" class="form-label">Recurring</label>
-            <input type="text" class="form-control" id="Recurring<?php echo $dailyreminderf['ReminderID']; ?>" name="Recurring" value="<?php echo $dailyreminderf['Recurring']; ?>">
+            <label for="Recurring<?php echo $dailyremindersf['ReminderID']; ?>" class="form-label">Recurring</label>
+            <input type="text" class="form-control" id="Recurring<?php echo $dailyremindersf['ReminderID']; ?>" name="Recurring" value="<?php echo $dailyremindersf['Recurring']; ?>">
           </div>
           
-          <input type="hidden" name="ReminderID" value="<?php echo $dailyreminderf['ReminderID']; ?>">
+          <input type="hidden" name="tdli" value="<?php echo $dailyremindersf['ReminderID']; ?>">
           <input type="hidden" name="actionType" value="Edit">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
